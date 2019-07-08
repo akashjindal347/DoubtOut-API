411f567155db64f1043d12983eeb4522167af895
Akash <akashjindal347@gmail.com>
2019-07-08 16:20:50 +0530

    added description field and improvised UI

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,7 @@ class Subject(models.Model):
     name = models.CharField(max_length = 100)
     link = models.CharField(max_length = 100)
     subject = models.CharField(max_length = 10,choices = SUBJECT_CHOICES,default = '')
+    description = models.TextField(default = '')
 
     def __str__(self):
         return str(self.name)
